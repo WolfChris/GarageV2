01157eb33c33469b7ff6d4433fbbda6f97aec173
Feliom05 <john_ay86@hotmail.com>
2017-10-24 17:04:21 +0200

    Add a checkout function!!

--- GarageV2/GarageV2/Models/ParkedVehicle.cs
+++ GarageV2/GarageV2/Models/ParkedVehicle.cs
@@ -10,12 +10,10 @@ namespace GarageV2.Models
     {
         public int Id { get; set; }
         public string Type { get; set; }
-        [Required]
         public string RegNo { get; set; }
         public string Color { get; set; }
         public string Brand { get; set; }
         public string Modell { get; set; }
-        [Range(0, 12)]
         public int NumberOfWheels { get; set; }
 
     }
